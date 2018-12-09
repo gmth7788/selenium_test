c4e9aa6f6456110c3ce659104976ea0d025bad20
gmth7788 <orchard2046@163.com>
2018-12-09 10:46:56 +0800

    愿景

--- xikao.py
+++ xikao.py
@@ -7,8 +7,39 @@ from bs4 import BeautifulSoup
 import os
 import sys
 
+def send_mail():
+    '''
+    发送邮件
+    :return:
+    '''
+    pass
 
-def download_with_RFBP(src_path, dest_path, total_size, temp_size):
+def send_weixin():
+    '''
+    发送微信
+    :return:
+    '''
+    pass
+
+def send_weibo():
+    '''
+    发送微博
+    :return:
+    '''
+    pass
+
+def send_baidu_netdisk():
+    '''
+    发送百度网盘
+    :return:
+    '''
+    pass
+
+
+
+
+def download_with_RFBP(src_path, dest_path,
+                       total_size, temp_size):
     '''
     断点续传(RFBP)
     从src_path偏移temp_size字节开始下载，添加到dest_path
@@ -16,7 +47,8 @@ def download_with_RFBP(src_path, dest_path, total_size, temp_size):
     temp_size已经下载的字节数
     '''
     headers = {'Range': 'bytes=%d-' % temp_size}
-    r = requests.get(src_path, stream=True, verify=False, headers=headers) 
+    r = requests.get(src_path, stream=True,
+                     verify=False, headers=headers)
     with open(dest_path, "ab") as f:
         for chunk in r.iter_content(chunk_size=1024):
             if chunk: 
@@ -25,8 +57,11 @@ def download_with_RFBP(src_path, dest_path, total_size, temp_size):
                 f.flush() 
                 ###这是下载实现进度显示#### 
                 done = int(50 * temp_size / total_size) 
-                sys.stdout.write("\r[%s%s] %d%%" % ('█' * done,
- ' ' * (50 - done), 100 * temp_size / total_size)) 
+                sys.stdout.write("\r[%s%s] %d%%" %
+                                 ('█' * done,
+                                  ' ' * (50 - done),
+                                  100 * temp_size /
+                                  total_size))
                 sys.stdout.flush() 
     print() # 避免上面\r 回车符
 
@@ -73,12 +108,15 @@ def download_file(src_path, dest_path, src_size):
             print("源文件%s太大了。")
             return            
 
-        print("src：{} {} {}".format(src_path,total_size, unit))
-        print("dst: {} {} {}".format(dest_path, temp_size, unit))
+        print("src：{} {} {}".format(src_path,total_size,
+                                    unit))
+        print("dst: {} {} {}".format(dest_path, temp_size,
+                                     unit))
 
 
     if (total_size > temp_size):
-        download_with_RFBP(src_path, dest_path, total_size, temp_size)
+        download_with_RFBP(src_path, dest_path,
+                           total_size, temp_size)
 
 
 def lookup_html(root_link, page_url):
@@ -97,16 +135,19 @@ def lookup_html(root_link, page_url):
             td_list = tr.find_all('td')
             a_list=tr.find_all('a')
             if a_list:
-                src_path_list.append(root_link+'/'+a_list[0].get('href'))
-                dest_path_list.append( "../xikao/{}-{}.zip".format(
-index, td_list[0].string))
+                src_path_list.append(root_link+'/'+
+                                     a_list[0].get('href'))
+                dest_path_list.append(
+                    "../xikao/{}-{}.zip".format(
+                        index, td_list[0].string))
                 s = ""
                 s = s.join(td_list[2].string)
                 src_size_list.append(s)
                 index+=1
 
-    for (src_path,dest_path, src_size) in zip(src_path_list, dest_path_list,
-src_size_list):
+    for (src_path,dest_path, src_size) in \
+            zip(src_path_list, dest_path_list,
+                src_size_list):
         try:
             download_file(src_path, dest_path, src_size)
             print("download OK.")
@@ -114,8 +155,10 @@ src_size_list):
             print('str(Exception):\t', str(Exception))
             print('str(e):\t\t', str(e))
             print('repr(e):\t', repr(e))
-            print('traceback.print_exc():' + traceback.print_exc())
-            print('traceback.format_exc():\n%s' % traceback.format_exc())
+            print('traceback.print_exc():' +
+                  traceback.print_exc())
+            print('traceback.format_exc():\n%s' %
+                  traceback.format_exc())
             print("download failed!")
         else:
             print("")
